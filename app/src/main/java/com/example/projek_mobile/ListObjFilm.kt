ecdea2f910180a163f6c2e6bd39ffe99b2e62853
mhdwahyuuu <mhdwahyuuu@gmail.com>
2021-02-15 17:11:21 +0700

    Update baru dari Wahyu

---
+++ app/src/main/java/com/example/projek_mobile/ListObjFilm.kt
@@ -0,0 +1,11 @@
+package com.example.projek_mobile
+
+class ListObjFilm(
+        var judul: String,
+        var rating: String,
+        var gambar: String,
+        var sinopsis: String,
+        var durasi: String,
+        var genre: String,
+        var sutradara: String
+    )
